dbf13e3da7f036b8224684148325f1d087b09ff5
Will Zegers <will.zegers@gmail.com>
2025-09-28 16:07:20 -0700

    Write to VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,11 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-const TEXT_COLOR: u8 = 0xd;
+mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = TEXT_COLOR;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
 
