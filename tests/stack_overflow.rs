71b0449ea568831e1ac9247edd23a9060bbc69be
Will Zegers <will.zegers@gmail.com>
2025-09-29 18:06:00 -0700

    Double fault handling

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use rust_os::{QemuExitCode, exit_qemu, serial_print, serial_println};
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    rust_os::gdt::init();
+    init_idt();
+
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    volatile::Volatile::new(0).read(); // prevent tail call elimination
+}
+
+fn init_idt() {
+    TEST_IDT.load();
+}
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(rust_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
